a43ab7851f97943d614fb8c54215e3fc60566ece
Brett Graham <brettgraham@gmail.com>
2015-07-01 17:33:15 -0400

    added test function for type conversions, I think string sending is broken

--- pycomando/protocols/command.py
+++ pycomando/protocols/command.py
@@ -17,39 +17,90 @@ types = {
     chr: (
         lambda v: struct.pack('<c', v),
         lambda bs: (1, struct.unpack('<c', bs[0])[0])),
-    ctypes.c_byte: (
-        lambda v: struct.pack('<c', chr(v.value)),
-        lambda bs: (1, struct.unpack('<c', bs[0])[0])),
-    ctypes.c_char: (
-        lambda v: struct.pack('<c', v),
-        lambda bs: (1, struct.unpack('<c', bs[0])[0])),
     int: (
         lambda v: struct.pack('<i', v),
         lambda bs: (2, struct.unpack('<h', bs[:2])[0])),
-    ctypes.c_int16: (
-        lambda v: struct.pack('<h', v.value),
-        lambda bs: (2, struct.unpack('<h', bs[:2])[0])),
-    ctypes.c_uint16: (
-        lambda v: struct.pack('<H', v.value),
-        lambda bs: (2, struct.unpack('<H', bs[:2])[0])),
-    ctypes.c_int32: (
-        lambda v: struct.pack('<i', v.value),
-        lambda bs: (4, struct.unpack('<i', bs[:4])[0])),
-    ctypes.c_uint32: (
-        lambda v: struct.pack('<I', v.value),
-        lambda bs: (4, struct.unpack('<I', bs[:4])[0])),
     float: (
         lambda v: struct.pack('<f', v),
         lambda bs: (4, struct.unpack('<f', bs[:4])[0])),
+    str: (
+        str,  # TODO I think this is wrong, should it pre-pend the length?
+        lambda bs: (bs[0], bs[1:1+ord(bs[0])])),
+    ctypes.c_byte: (
+        lambda v: struct.pack('<b', v.value),
+        lambda bs: (1, ctypes.c_byte(struct.unpack('<b', bs[0])[0]))),
+    ctypes.c_char: (
+        lambda v: struct.pack('<c', v.value),
+        lambda bs: (1, ctypes.c_char(struct.unpack('<c', bs[0])[0]))),
+    ctypes.c_int16: (
+        lambda v: struct.pack('<h', v.value),
+        lambda bs: (2, ctypes.c_int16(struct.unpack('<h', bs[:2])[0]))),
+    ctypes.c_uint16: (
+        lambda v: struct.pack('<H', v.value),
+        lambda bs: (2, ctypes.c_uint16(struct.unpack('<H', bs[:2])[0]))),
+    ctypes.c_int32: (
+        lambda v: struct.pack('<i', v.value),
+        lambda bs: (4, ctypes.c_int32(struct.unpack('<i', bs[:4])[0]))),
+    ctypes.c_uint32: (
+        lambda v: struct.pack('<I', v.value),
+        lambda bs: (4, ctypes.c_uint32(struct.unpack('<I', bs[:4])[0]))),
     ctypes.c_float: (
         lambda v: struct.pack('<f', v.value),
-        lambda bs: (4, struct.unpack('<f', bs[:4])[0])),
-    str: (
-        str,
-        lambda bs: (bs[0], bs[1:1+ord(bs[0])])),
+        lambda bs: (4, ctypes.c_float(struct.unpack('<f', bs[:4])[0]))),
 }
 types[ctypes.c_bool] = types[bool]
 
+
+def test_type_conversion():
+    c = ctypes
+    tests = [
+        (False, '\x00'),
+        (True, '\x01'),  # bool
+        (0, '\x00\x00\x00\x00'),
+        (1, '\x01\x00\x00\x00'),
+        (256, '\x00\x01\x00\x00'),
+        (-1, '\xff\xff\xff\xff'),  # int
+        (0.0, '\x00\x00\x00\x00'),
+        (1.0, '\x00\x00\x80?'),
+        (-1.0, '\x00\x00\x80\xbf'),  # float
+        #('\x00', '\x00'),
+        #('\xff', '\xff'),
+        #('abc', 'abc'),  # TODO fix string packing
+        #(';\x00\n\r,', ';\x00\n\r,'),  # string
+        (c.c_byte(0), '\x00'),
+        (c.c_byte(127), '\x7f'),
+        (c.c_byte(-128), '\x80'),  # c_byte
+        (c.c_char('\x00'), '\x00'),
+        (c.c_char('\xff'), '\xff'),  # c_char
+        (c.c_int16(0), '\x00\x00'),
+        (c.c_int16(1), '\x01\x00'),
+        (c.c_int16(-1), '\xff\xff'),
+        (c.c_int16(256), '\x00\x01'),  # c_int16
+        (c.c_uint16(0), '\x00\x00'),
+        (c.c_uint16(1), '\x01\x00'),
+        (c.c_uint16(256), '\x00\x01'),
+        (c.c_uint16(65535), '\xff\xff'),  # c_uint16
+        (c.c_int32(0), '\x00\x00\x00\x00'),
+        (c.c_int32(1), '\x01\x00\x00\x00'),
+        (c.c_int32(-1), '\xff\xff\xff\xff'),
+        (c.c_int32(256), '\x00\x01\x00\x00'),  # c_int32
+        (c.c_uint32(0), '\x00\x00\x00\x00'),
+        (c.c_uint32(1), '\x01\x00\x00\x00'),
+        (c.c_uint32(256), '\x00\x01\x00\x00'),
+        (c.c_uint32(4294967295), '\xff\xff\xff\xff'),  # c_uint32
+        (c.c_float(0.0), '\x00\x00\x00\x00'),
+        (c.c_float(1.0), '\x00\x00\x80?'),
+        (c.c_float(-1.0), '\x00\x00\x80\xbf'),  # c_float
+    ]
+    for v, r in tests:
+        pv = types[type(v)][0](v)
+        assert pv == r, "[%s pack error] %r != %r" % (type(v), pv, r)
+        uv = types[type(v)][1](r)[1]
+        if hasattr(v, 'value'):
+            e = uv.value == v.value
+        else:
+            e = uv == v
+        assert e, "[%s, unpack error] %r != %r" % (type(v), uv, v)
 
 
 class CommandProtocol(Protocol):
