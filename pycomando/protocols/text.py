4df3d028961531ab37e727acf689da1af02b775e
Brett Graham <brettgraham@gmail.com>
2015-12-02 12:45:29 -0500

    text: split bytestring at null char, use callbacks

--- pycomando/protocols/text.py
+++ pycomando/protocols/text.py
@@ -4,8 +4,20 @@ from .base import Protocol
 
 
 class TextProtocol(Protocol):
+    def __init__(self, comm=None, index=0):
+        Protocol.__init__(self, comm, index)
+        self.callbacks = []
+
+    def register_callback(self, func):
+        self.callbacks.append(func)
+
     def receive_message(self, bs):
-        pass
+        if '\x00' in bs:
+            s = bs.split('\x00')[0]
+        else:
+            s = bs
+        for cb in self.callbacks:
+            cb(s)
 
     def write(self, text):
         self.send_message(text)
